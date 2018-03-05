2e8e4fb82d4930fa655d179e39b568d714f0353c
Michał Urban <michalurban23@gmail.com>
2018-03-05 13:26:05 +0100

    Add data serializer

---
+++ src/main/java/com/codecool/lanpong/lanlayer/DataSerializer.java
@@ -0,0 +1,16 @@
+package com.codecool.lanpong.lanlayer;
+
+import com.codecool.lanpong.models.GameStatus;
+
+public class DataSerializer {
+
+    public static byte[] serialize(GameStatus status) {
+
+        return null;
+    }
+
+    public static GameStatus deserialize(byte[] data) {
+
+        return null;
+    }
+}
